8d2706b0b9ed27e77d7f21a6b3bc5c597f25614b
Viktor Nemeth <58767235+nemethviktor@users.noreply.github.com>
2024-04-28 18:22:25 +0100

    Initial commit [#2]

---
+++ Program.cs
@@ -0,0 +1,185 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using CommandLine;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace MP3AppenderNinja;
+
+public class Program
+{
+    // Load app with options parsed from args (ffmpeg path and the root folder to parse)
+    private class Options
+    {
+        [Option(shortName: 'f', longName: "ffmpegPath", Required = true, HelpText = "Full Path to FFMPEG.exe.",
+            Default = @"C:\Program Files\FFMpeg\bin\ffmpeg.exe")]
+        public string FfmpegPath { get; set; }
+
+        [Option(shortName: 'p', longName: "parseFolderPath", Required = true,
+            HelpText = "Root Folder to parse (recursive).")]
+        public string ParseFolderPath { get; set; }
+
+        [Option(shortName: 'k', longName: "keepOriginalFiles", Required = false,
+            HelpText = "Keep (aka don't delete) original files?", Default = false)]
+        public bool KeepOriginalFiles { get; set; }
+    }
+
+    private static void Main(string[] args)
+    {
+        string ffmpegPath = string.Empty;
+        string parseFolderPath = string.Empty;
+        bool keepOriginalFiles = false;
+
+        string exitMsg = string.Empty;
+        const char ffmpegSeparatorChar = '|';
+        const string ffmpegStringStart = " -y -i \"concat:"; // needed for ffmpeg to do our bidding
+        const string ffmpegStringEnd = "\" -acodec copy "; // needed for ffmpeg to do our bidding
+
+        // cls
+        Console.Clear();
+
+        // ascertain that the args seem valid, else -> exit.
+        Parser.Default.ParseArguments<Options>(args: args)
+              .WithParsed(action: o =>
+               {
+                   char[] trimChars = { '\\', ' ', '\"' };
+
+                   o.FfmpegPath = o.FfmpegPath.TrimEnd(trimChars: trimChars);
+                   o.ParseFolderPath = o.ParseFolderPath.TrimEnd(trimChars: trimChars);
+                   ffmpegPath = o.FfmpegPath;
+                   parseFolderPath = o.ParseFolderPath;
+                   keepOriginalFiles = o.KeepOriginalFiles;
+               });
+
+        if (!File.Exists(path: ffmpegPath)) exitMsg += Environment.NewLine + "FFMPEG path invalid.";
+
+        if (!Directory.Exists(path: parseFolderPath))
+            exitMsg += Environment.NewLine + $"ParseFolderPath ({parseFolderPath}) path invalid.";
+
+        if (exitMsg != string.Empty)
+        {
+            exitMsg += Environment.NewLine + "Exiting.";
+            Console.WriteLine(value: exitMsg);
+            Environment.Exit(exitCode: 1);
+        }
+        else
+        {
+            Console.WriteLine(value: $"Starting app with {ffmpegPath} for FFMPEG and {parseFolderPath} to be parsed.");
+        }
+
+        // get a list (array, really) of subfolders within root
+        string[] folders = Directory.GetDirectories(path: parseFolderPath, searchPattern: "*",
+            searchOption: SearchOption.AllDirectories);
+
+        // if root has no subfolders try the root itself
+        if (folders.Length == 0) folders = new string[1] { parseFolderPath };
+
+        // for each of the above folders run ffmpeg
+        foreach (string folder in folders) RunFFMpeg(folderPath: folder);
+
+        Console.WriteLine(value: "Process finished. Exiting." +
+                                 Environment.NewLine +
+                                 "If the app has been useful consider a donation at https://www.buymeacoffee.com/nemethv or https://www.paypal.com/donate/?hosted_button_id=R5GSBXW8A5NNN" +
+                                 Environment.NewLine +
+                                 "Alternatively, raise a ticket pls.");
+        return;
+
+        // This runs ffmpeg (shocker, right?)
+        // ReSharper disable once InconsistentNaming
+        void RunFFMpeg(string folderPath)
+        {
+            DirectoryInfo directoryInfo = new(path: folderPath);
+
+            // get a list of mp3s (actually this does a lot more things, check inside.)
+            string[] fileList = GetOrderedFileArray(folderPath: folderPath);
+
+            // basically if we already have 1 (no more) mp3 then there's nothing to do, no need to merge 1 file with itself.
+            if (fileList.Length > 1)
+            {
+                int ffmpegResult = RunExternalExe(fileName: ffmpegPath, arguments: ffmpegStringStart +
+                    string.Join(separator: ffmpegSeparatorChar,
+                        value: fileList) +
+                    ffmpegStringEnd +
+                    $" \"{folderPath + "\\" + directoryInfo.Name}" +
+                    ".mp3\"");
+                if (ffmpegResult == 0 &&
+                    !keepOriginalFiles)
+                    foreach (string fileName in fileList)
+                        File.Delete(path: fileName);
+            }
+        }
+
+        // hopefully obvious
+        int RunExternalExe(string fileName, string arguments)
+        {
+            Process process = new();
+
+            process.StartInfo.FileName = fileName;
+            if (!string.IsNullOrEmpty(value: arguments)) process.StartInfo.Arguments = arguments;
+
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.UseShellExecute = false;
+
+            process.StartInfo.RedirectStandardError = false;
+            process.StartInfo.RedirectStandardOutput = false;
+            StringBuilder stdOutput = new();
+            process.OutputDataReceived +=
+                (sender, dataReceivedEventArgs) =>
+                    stdOutput.AppendLine(value: dataReceivedEventArgs
+                       .Data);
+
+            try
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+            catch
+            {
+                // nothing
+            }
+
+            return process.ExitCode;
+        }
+
+        // see comments inside
+        string[] GetOrderedFileArray(string folderPath)
+        {
+            DirectoryInfo directoryInfo = new(path: folderPath);
+            List<string> fileListPadded = new();
+
+            // get every mp3 in the folder that's not the same as the foldername
+            // the above is to ascertain that we don't end up merging some aborted merge with itself. 
+            // also as to make sure it doesn't get deleted afterwards
+            IEnumerable<string> fileList = Directory
+                                          .EnumerateFiles(path: folderPath)
+                                          .Where(predicate: file =>
+                                               file.ToLower().EndsWith(value: "mp3") &&
+                                               !file.ToLower()
+                                                    .EndsWith(value: "\\" + directoryInfo.Name.ToLower() + ".mp3"));
+
+            // sorted dictionary where the padded name is the key
+            SortedDictionary<string, string> sortedFileDict = new();
+            foreach (string s in fileList)
+                sortedFileDict.Add(key: PadFileName(fileName: Path.GetFileNameWithoutExtension(path: s)),
+                    value: Path.GetFileNameWithoutExtension(path: s));
+
+            // return the original filenames in proper sorted order
+            foreach (KeyValuePair<string, string> keyValuePair in sortedFileDict)
+                fileListPadded.Add(item: Path.Combine(path1: folderPath, path2: keyValuePair.Value + ".mp3"));
+            return fileListPadded.ToArray();
+        }
+
+        // the logic here is that we take any fileName and within that take any consecutive batch of numeric chars
+        // ... and then pad it to 3. generally this should avoid issues with sorting crap like "chapter 1" vs "chapter 11".
+        // ... because we'll have chapter 001 and chapter 011. 
+        static string PadFileName(string fileName)
+        {
+            Regex regex = new(pattern: @"\d+");
+            string paddedFileName = regex.Replace(input: fileName,
+                evaluator: m => m.Value.PadLeft(totalWidth: 3, paddingChar: '0'));
+            return paddedFileName;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# MP3AppenderNinja
- MP3 Joiner/Appender with smart(ish) sorting.
+# Welcome to MP3AppendNinja
+## What This Is/Does (?)
+Great question! This is a console app (no GUI) for Windows that appends mp3s together within a loop of subfolders. In somewhat plainer  English that means you designate a "root" folder (say `C:\audiobooks`) and then in each of the subfolders (such as `C:\audiobooks\Cryptonomicon`, `C:\audiobooks\Snowcrash` etc) the script will parse those subfolders and get all your mp3s into one merged file per folder.
+Things it may also do/certainly does: 
+- it will delete your original files (see further below)
+- it will create an output file that has the same name as the folder (so following from the logic above if you have `C:\audiobooks\Snowcrash\chapter 1.mp3` and `C:\audiobooks\Snowcrash\chapter 2.mp3` then you'll eventually end up with a `C:\audiobooks\Snowcrash\Snowcrash.mp3` and nothing else)
+
+## Download & Install // Requirements
+
+- There's nothing to install. Just get the zip file from Releases, unzip and run the .exe (read below for arguments/howto)
+- You'll need Windows 7 x64 or newer and .NET 8
+    - You can get .NET 8 at https://dotnet.microsoft.com/en-us/download/dotnet/thank-you/runtime-8.0.4-windows-x64-installer should you need it.
+- You'll need FFMPEG --> https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z (you'll need to unzip [un7z?] that // in all fairness any FFMPEG version should do)
+- The app is unsigned. This is because a certificate costs in the vicinity of £250 per year but the app is free, and I don't particularly feel like splurging out on this at the moment.
+	- Due to the lack of a signed certificate, when running the app SmartScreen may complain that the app is unsafe. SmartScreen is meant to be called StupidScreen but MS made a typo there.
+
+## How to use
+
+As mentioned above you need to run the exe file. This is a console app (aka no GUI) and the usage is `MP3AppenderNinja.exe -f fullPathToFFMPEG.exeInDoubleQuotes -p FullPathToRootFolderToParseInDoubleQuotes`, so for example in real terms that would be `MP3AppenderNinja.exe -f "d:\Downloads\ffmpeg-6.0-full_build\bin\ffmpeg.exe" -p "d:\Downloads\Warhammer 40k The Horus Heresy - complete\"`
+
+## Things to Note
+
+- The main _to-note_ is that the script isn't really foolproof and it's unlikely I'll ever cater for that. The assumption is that the files aren't locked, they aren't read-only [in other words turn off your torrent client], the script (technically the user running it) has read/write access to whatever folder and so on. 
+	- I'd generally suggest not running the script with a target on a Google Drive/OneDrive folder because those clients can *really* interfere w/ write-operations.
+	
+### Sorting
+
+- There's some logic to attempt re-sort files. The issue is that if you have files like `whatever chapter 1.mp3` and `whatever chapter 2.mp3` and `whatever chapter 10.mp3` and `whatever chapter 11.mp3` then you'll have a sorting problem because `1` and `10` are strings, not numbers in the world of Windows so there's a fair bit of code that goes in to changing anything that looks like a number to a 3-padded (aka `1` becomes `001` --> `whatever chapter 001.mp3`). This works in most cases.
+	- As I've written the script isn't idiot-proof. I've seen some files that were `000 intro.mp3` and then `000 quotes.mp3`; in such cases alphabetic sort will kick in so basically make sure whatever you're appending is following some form of reasonable naming procedure.
+
+### Errors
+
+- The app only works with mp3 files, this is a feature, not a bug.
+- It's likely you'll see something in the output such as `Audio packet of size 215 (starting with 00610069...) is invalid, writing it anyway.` - this isn't a bug either, ffmpeg does it own things
+- The duration of the output file may be misreported by some players. Again this is a thing of ffmpeg but from what I gather the alternatives don't do a better job on the merge either.
